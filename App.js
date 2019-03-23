4874b9eba709f685f211fb0ca7bcca3a8a148e89
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-22 21:10:24 -0300

    Formatar cena principal.

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View
+} from 'react-native';
+
+import Logo from './src/images/logo.png';
+import btnJogarImage from './src/images/botao_jogar.png';
+import btnSobreJogoImage from './src/images/sobre_jogo.png';
+import btnOutrosJogosImage from './src/images/outros_jogos.png';
 
 export default class App extends React.Component {
   render() {
+    const { principal, apresentacao, rodape } = estilos;
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={principal}>
+        <View style={apresentacao}>
+          <Image source={Logo} />
+          <Image source={btnJogarImage} />
+        </View>
+        <View style={rodape}>
+          <Image source={btnSobreJogoImage} />
+          <Image source={btnOutrosJogosImage} />
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+const estilos = StyleSheet.create({
+  principal: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#61bd8c',
   },
+  apresentacao: {
+    flex: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rodape: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
 });
