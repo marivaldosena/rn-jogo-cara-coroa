83ed1f14b256b3094582f9e2bf3a16daafe67232
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-22 22:42:27 -0300

    Criar cena de resultado.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Router, Scene, Stack } from 'react-native-router-flux';
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
 import OutrosJogos from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
 
 export default class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ export default class App extends React.Component {
           <Scene key='principal' component={Principal} initial title='Cara ou Coroa' />
           <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
           <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
+          <Scene key='resultado' component={Resultado} title='Resultado' />
         </Stack>
       </Router>
     );
---
+++ src/components/Resultado.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Resultado = () => {
+  return (
+    <View>
+      <Text>Resultado</Text>
+    </View>
+  );
+};
+
+export default Resultado;
