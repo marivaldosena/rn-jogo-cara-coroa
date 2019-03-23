5e02332561f9770e55628fa723b6acaac7b25101
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-23 19:25:59 -0300

    Refatorar código e excluir arquivo index.js.

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Router, Scene, Stack } from 'react-native-router-flux';
-
-import Principal from './src/components/Principal';
-import SobreJogo from './src/components/SobreJogo';
-import OutrosJogos from './src/components/OutrosJogos';
-import Resultado from './src/components/Resultado';
+import Rotas from './src/Rotas';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Stack key='root' headerLayoutPreset='center'>
-          <Scene key='principal' component={Principal} initial title='Cara ou Coroa' />
-          <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
-          <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
-          <Scene key='resultado' component={Resultado} title='Resultado' />
-        </Stack>
-      </Router>
+      <Rotas />
     );
   }
 }
